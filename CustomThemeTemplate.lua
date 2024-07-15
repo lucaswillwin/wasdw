2e9a2a4e57ee14d20ad9c17b41c1739a118e548a
Lucas <98550819+lucaswillwin@users.noreply.github.com>
2024-07-15 17:39:01 -0400

    Add files via upload

---
+++ CustomThemeTemplate.lua
@@ -0,0 +1,55 @@
+Custom_Finity_Theme = {}
+
+-- Please note that when you add your own themes, You will need to have the name given be EXACTLY as the theme name.
+
+Custom_Finity_Theme.CustomThemes = {
+
+	["Example"] = { -- this is dark mode, customize it all you want.
+
+		main_container = Color3.fromRGB(32, 32, 33),
+		separator_color = Color3.fromRGB(63, 63, 65),
+
+		text_color = Color3.fromRGB(206, 206, 206),
+
+		category_button_background = Color3.fromRGB(63, 62, 65),
+		category_button_border = Color3.fromRGB(72, 71, 74),
+
+		checkbox_checked = Color3.fromRGB(132, 255, 130),
+		checkbox_checked_inner = Color3.fromRGB(141, 245, 139),
+		checkbox_outer = Color3.fromRGB(84, 81, 86),
+		checkbox_inner = Color3.fromRGB(132, 132, 136),
+
+		slider_color = Color3.fromRGB(177, 177, 177),
+		slider_color_sliding = Color3.fromRGB(132, 255, 130),
+		slider_background = Color3.fromRGB(88, 84, 90),
+		slider_text = Color3.fromRGB(177, 177, 177),
+
+		textbox_background = Color3.fromRGB(103, 103, 106),
+		textbox_background_hover = Color3.fromRGB(137, 137, 141),
+		textbox_text = Color3.fromRGB(195, 195, 195),
+		textbox_text_hover = Color3.fromRGB(232, 232, 232),
+		textbox_placeholder = Color3.fromRGB(135, 135, 138),
+
+		dropdown_background = Color3.fromRGB(88, 88, 91),
+		dropdown_text = Color3.fromRGB(195, 195, 195),
+		dropdown_text_hover = Color3.fromRGB(232, 232, 232),
+		dropdown_scrollbar_color = Color3.fromRGB(118, 118, 121),
+		dropdown_scrollbar_thickness = 4,
+
+		button_background = Color3.fromRGB(103, 103, 106),
+		button_background_hover = Color3.fromRGB(137, 137, 141),
+		button_background_down = Color3.fromRGB(70, 70, 81),
+
+		scrollbar_color = Color3.fromRGB(118, 118, 121),
+
+		notification_warn = Color3.fromRGB(236, 129, 44),
+		notification_success = Color3.fromRGB(139, 217, 45),
+		notification_info = Color3.fromRGB(206, 206, 206),
+
+		NotifSound = "FinityGUI/assets/NotifSound.wav",
+
+	},
+
+}
+
+return Custom_Finity_Theme
